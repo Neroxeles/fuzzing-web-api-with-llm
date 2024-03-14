b4d1ddb7eb07eae0c12a95202f69591ce917df5f
Robert Schönfeld <schoenfeld.robert.98@gmail.com>
2024-03-14 18:44:31 +0100

    added offload_folder

--- Fuzzer/model/StarCoder.py
+++ Fuzzer/model/StarCoder.py
@@ -83,7 +83,7 @@ class StarCoder:
     self.device = device
     self.tokenizer = AutoTokenizer.from_pretrained(checkpoint)
     self.model = (
-      GPTBigCodeForCausalLM.from_pretrained(checkpoint, device_map="auto", torch_dtype=torch.bfloat16)
+      GPTBigCodeForCausalLM.from_pretrained(checkpoint, device_map="auto", offload_folder="offload", torch_dtype=torch.bfloat16)
                               # .to(torch.bfloat16) # accepts floating point or complex dtypes
                               # .to(self.device)
     )
